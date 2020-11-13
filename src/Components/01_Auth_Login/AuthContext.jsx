099a4aa076ff116964cdcd7b14a64ddb0e5487bc
Shawn Becker <shawn@yptokey.com>
2020-11-13 13:19:05 +0100

    prepared auth contaxt

--- src/Components/01_Auth_Login/AuthContext.jsx
+++ src/Components/01_Auth_Login/AuthContext.jsx
@@ -3,7 +3,17 @@ import React, { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(true);
+
+  // const checkAuth = () => {
+  //   const token = localStorage.getItem("token");
+  //   const refreshToken = localStorage.getItem("refreshToken");
+  //   if (!token || !refreshToken) {
+  //     return false;
+  //   }
+
+  //   return true;
+  // };
 
   return (
     <AuthContext.Provider value={[isAuth, setIsAuth]}>
