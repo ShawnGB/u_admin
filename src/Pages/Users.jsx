5f07a84adc91e771406e6a7aaa29fa21930be6ff
Shawn Becker <shawn@yptokey.com>
2020-11-13 19:33:54 +0100

    add user dialog

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Dialog from "@material-ui/core/Dialog";
 import axios from "axios";
 import UsersListing from "../Components/03_Users/UsersListing";
+import AddUsersFrom from "../Components/03_Users/AddUsersFrom";
 
 export default function Users() {
   const [users, setUsers] = useState();
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
     axios
@@ -19,8 +22,18 @@ export default function Users() {
 
   return (
     <div style={{ height: "70vh" }}>
+      <Dialog
+        onBackdropClick={() => setOpenDialog(false)}
+        fullWidth={true}
+        open={openDialog}
+        children={<AddUsersFrom closeDialog={() => setOpenDialog(false)} />}
+      ></Dialog>
       <div style={{ height: "4em", allingItems: "center" }}>
-        <input type="button" value="Add User" style={{}} />
+        <input
+          type="button"
+          value="Add User"
+          onClick={() => setOpenDialog(true)}
+        />
       </div>
       <div>
         <UsersListing users={users} />
